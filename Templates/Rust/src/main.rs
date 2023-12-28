88f3961e78d14822fe3befbcf6449f60fa84bf14
Michael Ott <mott@microsoft.com>
2023-12-28 14:16:03 -0800

    Update Rust project template

--- Templates/Rust/src/main.rs
+++ Templates/Rust/src/main.rs
@@ -1,3 +1,23 @@
+use std::{
+    fs::File,
+    io::BufRead,
+    io::BufReader,
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+       .map(|l| l.expect("Could not parse line"))
+       .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut result = 0;
+    let lines = lines_from_file("input.txt");
+
+    // Implement
+
+    println!("Result: {}", result);
 }
