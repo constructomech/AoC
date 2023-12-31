664965dddc7766905f254c8f9f821dc2b703d900
Michael Ott <mott@microsoft.com>
2023-12-31 15:36:29 -0800

    2020 Day 6

---
+++ 2020/day_06/2/src/main.rs
@@ -0,0 +1,50 @@
+use std::{
+    fs::File,
+    io::BufRead,
+    io::BufReader,
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+       .map(|l| l.expect("Could not parse line"))
+       .collect()
+}
+
+fn count_bits(num: u32) -> u32 {
+    let mut result = 0;
+    let mut n = num;
+
+    while n > 0 {
+        result += n & 1;
+        n >>= 1;
+    }
+
+    result
+}
+
+fn main() {
+    let mut result = 0;
+    let mut lines = lines_from_file("input.txt");
+    lines.push(String::new()); // To make the loop iteration simpler
+
+    let mut group_answers: u32 = !0;
+
+    for line in lines {
+        if line == "" {
+            result += count_bits(group_answers);
+            group_answers = !0;
+            continue;
+        }
+        let mut answers = 0;
+        for c in line.chars() {
+            let bit_num : u32 = c as u32 - 'a' as u32;
+            answers |= 1 << bit_num;
+        }
+        group_answers &= answers;
+    }
+
+    println!("Result: {}", result);
+}
