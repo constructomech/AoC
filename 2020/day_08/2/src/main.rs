58d02971be44dfa8890f549de51f8ce3c942f95a
Michael Ott <mott@microsoft.com>
2024-01-03 18:10:17 -0800

    2020 Day 8

--- 2020/day_08/2/src/main.rs
+++ 2020/day_08/2/src/main.rs
@@ -20,12 +20,19 @@ struct Instruction {
     value: i32,
 }
 
-fn run_program(instructions: &Vec<Instruction>, mut instruction_pointer : i32) -> i32 {
+fn run_program(instructions: &Vec<Instruction>) -> Result<i32, bool> {
+    let mut instruction_pointer : i32 = 0;
     let mut accumulator = 0;
     let mut visited_instructions : HashSet::<i32> = HashSet::new();
 
     while !visited_instructions.contains(&instruction_pointer) {
         visited_instructions.insert(instruction_pointer);
+
+        if instruction_pointer < 0 || instruction_pointer as usize > instructions.len() {
+            return Err(false);
+        } else if instruction_pointer as usize == instructions.len() {
+            return Ok(accumulator);
+        } 
 
         let instruction = &instructions[instruction_pointer as usize];
         match instruction.name.as_str() {
@@ -39,7 +46,22 @@ fn run_program(instructions: &Vec<Instruction>, mut instruction_pointer : i32) -
         }
     }
 
-    return accumulator;
+    return Err(false);
+}
+
+fn generate_variations(instructions: &Vec<Instruction>) -> impl Iterator<Item = Vec<Instruction>> + '_ {
+    (0..instructions.len()).flat_map(move |i| {
+        let mut variation = instructions.clone();
+        let instruction = &variation[i];
+
+        match instruction.name.as_str() {
+            "nop" => variation[i].name = "jmp".to_string(),
+            "jmp" => variation[i].name = "nop".to_string(),
+            _ => return None, // Skip non-"nop" and non-"jmp" instructions
+        }
+
+        Some(variation)
+    })
 }
 
 fn main() {
@@ -54,6 +76,10 @@ fn main() {
         instructions.push(Instruction{name: instruction.to_string(), value: value});
     }
 
-    let result = run_program(&instructions, 0);
-    println!("Result: {}", result);
+    for variation in generate_variations(&instructions) {
+        if let Ok(result) = run_program(&variation) {
+            println!("Result: {}", result);
+            break;
+        }
+    }
 }
