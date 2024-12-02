1bf6bad47ef3401131651d3003c010c4369a56fd
Michael Ott <mott@microsoft.com>
2024-12-02 10:41:43 -0800

    2024 day 2 part 1.

---
+++ 2024/day_02/1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+
+    int result = 0;
+    var lists = input.Select(x => x.Split(' ').Select(int.Parse).ToList()).ToList();
+
+    foreach (var list in lists) {
+
+        bool safe = true;
+        bool? ascending = null;
+
+        for (int i = 1; i < list.Count; i++) {
+            bool localAscending = list[i] > list[i - 1];
+            int diff = Math.Abs(list[i] - list[i - 1]);
+
+            if (ascending == null) {
+                ascending = localAscending;
+            }
+
+            if (localAscending != ascending || diff < 1 || diff > 3) {
+                safe = false;
+                break;
+            }
+        }
+
+        if (safe) {
+            result++;
+        }
+    }
+    Console.WriteLine(result);
+}
