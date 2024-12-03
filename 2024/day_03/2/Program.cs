d97b11505cd968bd01c436fb7cd6efc700c2b751
Michael Ott <mott@microsoft.com>
2024-12-03 15:24:41 -0800

    2024 day 3 part 2.

---
+++ 2024/day_03/2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+
+    int result = 0;
+
+    var all = input.Aggregate((a, b) => a + b);
+
+    var parts = all.Split("do()");
+
+    foreach (var doLine in parts) {
+        var doParts = doLine.Split("don't()");
+        var check = doParts[0];
+
+        string pattern = @"mul\((\d+),(\d+)\)";
+        MatchCollection matches = Regex.Matches(check, pattern);
+
+        foreach (Match match in matches) {
+            var a = int.Parse(match.Groups[1].Value);
+            var b = int.Parse(match.Groups[2].Value);
+            result += a * b;
+        }
+    }
+
+    Console.WriteLine(result);
+}
