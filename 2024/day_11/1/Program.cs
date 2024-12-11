038b59ea0a06b16b2bde01e0ce56bcf4665ce439
Michael Ott <michaelott@Michaels-MacBook-Pro.local>
2024-12-11 06:58:54 +0100

    2024 Day 11

---
+++ 2024/day_11/1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+
+    var stones = input[0].Split(' ').ToList();
+
+    // Blink 25 times
+    for (var i = 0; i < 25; i++) {
+
+        for (var stoneIdx = 0; stoneIdx < stones.Count; stoneIdx++) {
+            if (stones[stoneIdx] == "0") {
+                stones[stoneIdx] = "1";
+
+            } else if (stones[stoneIdx].Length % 2 == 0) {
+                var lhs = stones[stoneIdx].Substring(0, stones[stoneIdx].Length / 2);
+                var rhs = stones[stoneIdx].Substring(stones[stoneIdx].Length / 2);
+                while (rhs[0] == '0') {
+                    if (rhs == "0") break;
+                    rhs = rhs.Substring(1);
+                }
+                stones[stoneIdx] = lhs;
+                stoneIdx++;
+                stones.Insert(stoneIdx, rhs);
+                
+            } else {
+                var num = Convert.ToDecimal(stones[stoneIdx]);
+                num *= 2024;
+                stones[stoneIdx] = num.ToString();             
+            }
+        }
+
+    }
+
+    result = stones.Count;
+    Console.WriteLine($"Result: {result}");
+}
---
+++ 2024/day_11/2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+long Blink(string input, int iter, Dictionary<(string input, int iter), long> cache) {
+    long result = 0;
+
+    long answer;
+    if (cache.TryGetValue((input, iter), out answer)) {
+        return answer;
+    }
+
+    if (iter == 75) {
+        result = 1;
+    } else {
+        if (input == "0") {
+            result += Blink("1", iter + 1, cache);
+
+        } else if (input.Length % 2 == 0) {
+            var lhs = input.Substring(0, input.Length / 2);
+            var rhs = input.Substring(input.Length / 2);
+            while (rhs[0] == '0') {
+                if (rhs == "0") break;
+                rhs = rhs.Substring(1);
+            }
+
+            result += Blink(lhs, iter + 1, cache);
+            result += Blink(rhs, iter + 1, cache);
+            
+        } else {
+            var num = Convert.ToDecimal(input);
+            num *= 2024;
+            result += Blink(num.ToString(), iter + 1, cache);
+        }
+    }
+
+    cache.TryAdd((input, iter), result);
+
+    return result;
+}
+
+void Run(string[] input) {
+    var result = 0L;
+
+    var stones = input[0].Split(' ').ToList();
+
+    var cache = new Dictionary<(string input, int iter), long>(); // Input string & iter to final count
+
+    for (var stoneIdx = 0; stoneIdx < stones.Count; stoneIdx++) {
+        result += Blink(stones[stoneIdx], 0, cache);
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
