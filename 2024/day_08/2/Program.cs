86d3bb34c1465ac08cc571f1d06defe710a0048f
Michael Ott <michaelott@Michaels-MacBook-Pro.local>
2024-12-11 06:59:29 +0100

    2024 Day 8 part 2

---
+++ 2024/day_08/2/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+var CardinalAdjacent = ImmutableList.Create<Vec2>(new(1, 0), new(0, 1), new(-1, 0), new(0, -1));
+var DiagonallyAdjacent = ImmutableList.Create<Vec2>(new(-1, -1), new(-1, 1), new(1, 1), new(1, -1));
+var AllAdjacent = CardinalAdjacent.AddRange(DiagonallyAdjacent);
+
+void Run(string[] input) {
+    var width = input[0].Length;
+    var height = input.Length;
+    var antennas = new Dictionary<char, List<Vec2>>();
+
+    for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+            var node = input[y][x];
+
+            if (node != '.') {
+                antennas.TryAdd(node, new List<Vec2>());
+                antennas[node].Add(new Vec2(x, y));
+            }
+        }
+    }
+
+    var antinodes = new List<Vec2>();
+
+    foreach ((_, var points) in antennas) {
+        foreach (var permutation in GetPermutations(points, 2)) {
+            var p1 = permutation.First();
+            var p2 = permutation.Last();
+            var vecBetween = p2 - p1; 
+
+            for (var r = p1; r.X >= 0 && r.Y >= 0 && r.X < width && r.Y < height; r -= vecBetween) {
+                antinodes.Add(r);
+            }
+
+            for (var r = p2; r.X >= 0 && r.Y >= 0 && r.X < width && r.Y < height; r += vecBetween) {
+                antinodes.Add(r);
+            }
+        }
+    }
+
+    var result = 0;
+
+    for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+            var node = input[y][x];
+
+            if (antinodes.Contains(new Vec2(x, y))) {
+                node = '#';
+                result++;
+            }
+
+            Console.Write(node);
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
+
+
+// MATH helpers
+
+static int gcf(int a, int b) {
+    while (b != 0) {
+        var temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+static int lcm(int a, int b) => (a / gcf(a, b)) * b;
+
+// For curve fitting, see the MathNet.Numerics package
+
+
+// PERMUTATIONS
+
+static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> items, int count) {
+    var i = 0;
+    foreach(var item in items) {
+        if(count == 1) {
+            yield return new T[] { item };
+        }
+        else {
+            foreach(var result in GetPermutations(items.Skip(i + 1), count - 1)) {
+                yield return new T[] { item }.Concat(result);
+            }
+        }
+        i++;
+    }
+}
+
+
+// DIRECTION helpers
+
+Direction ParseDirection(char c) {
+    return c switch {
+        '^' => Direction.Up,
+        'v' => Direction.Down,
+        '<' => Direction.Left,
+        '>' => Direction.Right,
+        _ => throw new Exception("Invalid direction")
+    };
+}
+
+Vec2 OffsetFromDirection(Direction direction) {
+    return direction switch {
+        Direction.Up => new(0, -1),
+        Direction.Down => new(0, 1),
+        Direction.Left => new(-1, 0),
+        Direction.Right => new(1, 0),
+        _ => throw new Exception("Invalid direction")
+    };
+}
+
+enum Direction { Up, Down, Left, Right }
+
+
+// COMPARERS 
+
+// For a priority queue optimizing for highest cost
+class InverseComparer : IComparer<int> {
+    public int Compare(int lhs, int rhs) => rhs.CompareTo(lhs);
+}
+
+// For using List<string> as a key in a dictionary or set
+class ListComparer<T> : IEqualityComparer<List<T>>
+{
+    public bool Equals(List<T>? x, List<T>? y) => x == null || y == null ? false : x.SequenceEqual(y);
+
+    public int GetHashCode(List<T> obj) {
+        var hashcode = 0;
+        foreach (T t in obj) {
+            var lineHash = t != null ? t.GetHashCode() : 0;
+            hashcode ^= lineHash + BitConverter.ToInt32(_hashSalt) + (hashcode << 6) + (hashcode >> 2);
+        }
+        return hashcode;
+    }
+
+    private static readonly Byte[] _hashSalt = BitConverter.GetBytes(0x9e3779b9);
+}
+
+
+// VECTORS
+
+public record Vec2 (int X, int Y) {
+    public static Vec2 FromString(string s) {
+        var parts = s.Split(',');
+        return new(int.Parse(parts[0]), int.Parse(parts[1]));
+    }
+
+    public static Vec2 operator +(Vec2 a, Vec2 b) => new(a.X + b.X, a.Y + b.Y);
+
+    public static Vec2 operator -(Vec2 a, Vec2 b) => new(a.X - b.X, a.Y - b.Y);
+
+    public static Vec2 operator *(Vec2 a, int b) => new(a.X * b, a.Y * b);
+}
+
+public record Vec3 (int X, int Y, int Z) {
+    public static Vec3 FromString(string s) {
+        var parts = s.Split(',');
+        return new(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]));
+    }
+
+    public static Vec3 operator +(Vec3 a, Vec3 b) => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+    public static Vec3 operator -(Vec3 a, Vec3 b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+    public static Vec3 operator *(Vec3 a, int b) => new(a.X * b, a.Y * b, a.Z * b);
+}
