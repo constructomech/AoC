339c5185c3f09bcb45313a5366a2c01a45a315e8
Michael Ott <michaeljamesott@gmail.com>
2024-12-22 00:09:16 -0800

    2024 day 21 part 2 - different appraoches for comparison.

---
+++ 2024/day_21/2.1/Program.cs
@@ -0,0 +1,171 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+var solution = new Solution();
+solution.Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+public sealed class Solution
+{
+    private static readonly Pad Numpad = Pad.Parse(flat: "*0A123456789", cols: 3, skip: '*');
+    private static readonly Pad Dirpad = Pad.Parse(flat: "<v>*^A", cols: 3, skip: '*');
+    private static readonly Dictionary<(Vec2, Vec2, int), long> Memo = new();
+
+    private readonly record struct State(Vec2 Pos, string Sequence)
+    {
+        public State Up() => new(Pos + Vec2.Up, $"{Sequence}^");
+        public State Down() => new(Pos + Vec2.Down, $"{Sequence}v");
+        public State Left() => new(Pos + Vec2.Left, $"{Sequence}<");
+        public State Right() => new(Pos + Vec2.Right, $"{Sequence}>");
+    }
+
+    public void Run(string[] input)
+    {
+        // var part1 = input.Select(code => GetComplexity(code, 2)).Sum();
+        // var part2 = input.Select(code => GetComplexity(code, 25)).Sum();
+
+        // Console.WriteLine($"Part1: {part1}");
+        // Console.WriteLine($"Part2: {part2}");
+
+        var result = 0L;
+        foreach (var code in input) {
+            var complexity = GetComplexity(code, 2);
+            result += complexity;
+        }
+        Console.WriteLine(result);
+
+    }
+
+    private static long GetComplexity(string sequence, int numDirPads)
+    {
+        var total = 0L;
+        var start = Numpad.KeyMap['A'];
+
+        foreach (var end in sequence.Select(key => Numpad.KeyMap[key]))
+        {
+            total += GetSequenceCost(start, end, pad: Numpad, robot: numDirPads);
+            start = end;
+        }
+
+        return total * long.Parse(sequence.Substring(0, sequence.Length - 1));
+    }
+
+    private static long GetSequenceCost(Vec2 start, Vec2 end, Pad pad, int robot)
+    {
+        if (pad == Dirpad && Memo.TryGetValue((start, end, robot), out var cached))
+        {
+            return cached;
+        }
+
+        var result = long.MaxValue;
+        var queue = new Queue<State>([new State(Pos: start, Sequence: "")]);
+
+        while (queue.Count != 0)
+        {
+            var state = queue.Dequeue();
+            if (!pad.PosMap.ContainsKey(state.Pos))
+            {
+                continue;
+            }
+
+            if (state.Pos == end)
+            {
+                var remote = pad == Dirpad;
+                var target = remote ? robot - 1 : robot;
+
+                result = long.Min(result, CalculateRobotCost($"{state.Sequence}A", robot: target));
+                continue;
+            }
+
+            if (state.Pos.Y < end.Y) queue.Enqueue(state.Up());
+            if (state.Pos.Y > end.Y) queue.Enqueue(state.Down());
+            if (state.Pos.X < end.X) queue.Enqueue(state.Right());
+            if (state.Pos.X > end.X) queue.Enqueue(state.Left());
+        }
+
+        if (pad == Dirpad) Memo[(start, end, robot)] = result;
+        return result;
+    }
+
+    private static long CalculateRobotCost(string sequence, int robot)
+    {
+        if (robot == 0)
+        {
+            return sequence.Length;
+        }
+
+        var total = 0L;
+        var start = Dirpad.KeyMap['A'];
+
+        foreach (var end in sequence.Select(key => Dirpad.KeyMap[key]))
+        {
+            total += GetSequenceCost(start, end, pad: Dirpad, robot);
+            start = end;
+        }
+
+        return total;
+    }
+}
+
+public class Pad
+{
+    public Dictionary<Vec2, char> PosMap { get; }
+    public Dictionary<char, Vec2> KeyMap { get; }
+
+    private Pad(Dictionary<Vec2, char> posMap)
+    {
+        PosMap = posMap;
+        KeyMap = posMap.ToDictionary(
+           keySelector: kvp => kvp.Value,
+           elementSelector: kvp => kvp.Key);
+    }
+
+    public static Pad Parse(string flat, int cols, char skip)
+    {
+        var keys = new Dictionary<Vec2, char>();
+        var rows = flat.Length / cols;
+
+        for (var y = 0; y < rows; y++)
+            for (var x = 0; x < cols; x++)
+            {
+                var pos = new Vec2(x, y);
+                var key = flat[y * cols + x];
+
+                if (key != skip)
+                {
+                    keys[pos] = key;
+                }
+            }
+
+        return new Pad(keys);
+    }
+}
+
+public record Vec2(int X, int Y)
+{
+    public static Vec2 FromString(string s)
+    {
+        var parts = s.Split(',');
+        return new(int.Parse(parts[0]), int.Parse(parts[1]));
+    }
+
+    public static Vec2 Up { get => new(0, -1); }
+
+    public static Vec2 Down { get => new(0, 1); }
+
+    public static Vec2 Left { get => new(-1, 0); }
+
+    public static Vec2 Right { get => new(1, 0); }
+
+
+    public static Vec2 operator +(Vec2 a, Vec2 b) => new(a.X + b.X, a.Y + b.Y);
+
+    public static Vec2 operator -(Vec2 a, Vec2 b) => new(a.X - b.X, a.Y - b.Y);
+
+    public static Vec2 operator *(Vec2 a, int b) => new(a.X * b, a.Y * b);
+}
---
+++ 2024/day_21/2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System.Diagnostics;
+
+
+string AllDirs = "><^vA";
+string AllNums = "0123456789A";
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+List<List<char>> GetDirs(char from, char to) {
+    if (from == to) return new List<List<char>> { new List<char>() };
+
+    var (rfrom, cfrom) = PadPos(from);
+    var (rto, cto) = PadPos(to);
+
+    int dr = rto - rfrom;
+    int dc = cto - cfrom;
+
+    var first = new List<char>();
+    if (dr != 0) {
+        var s = dr < 0 ? '^' : 'v';
+        first.AddRange(Enumerable.Repeat(s, Math.Abs(dr)));
+    }
+    if (dc != 0) {
+        var s = dc < 0 ? '<' : '>';
+        first.AddRange(Enumerable.Repeat(s, Math.Abs(dc)));
+    }
+
+    bool isDir = AllDirs.Contains(from) || AllDirs.Contains(to);
+    bool isNum = AllNums.Contains(from) || AllNums.Contains(to);
+
+    if (cto == 0 && dr == 1 && isDir || isNum && rfrom == 0 && cto == 0)
+        return new List<List<char>> { first };
+
+    var rev = new List<char>(first);
+    rev.Reverse();
+
+    if (cfrom == 0 && dr == -1 && isDir || isNum && cfrom == 0 && rto == 0 || dr != 0 && dc != 0)
+        return new List<List<char>> { first, rev };
+
+    return new List<List<char>> { first };
+}
+
+(int, int) PadPos(char x) => x switch {
+    '<' => (1, 0),
+    '^' => (0, 1),
+    'A' => (0, 2),
+    'v' => (1, 1),
+    '>' => (1, 2),
+    '0' => (0, 1),
+    '1' => (-1, 0),
+    '2' => (-1, 1),
+    '3' => (-1, 2),
+    '4' => (-2, 0),
+    '5' => (-2, 1),
+    '6' => (-2, 2),
+    '7' => (-3, 0),
+    '8' => (-3, 1),
+    '9' => (-3, 2),
+    _ => throw new InvalidOperationException("padPos")
+};
+
+Dictionary<string, decimal> GetTrivialPrices() {
+
+    var prices = new Dictionary<string, decimal>();
+    foreach (var from in AllDirs)
+        foreach (var to in AllDirs)
+            prices[string.Concat(from, to)] = 1;
+    return prices;
+}
+
+decimal TotalPrice(string bs, Dictionary<string, decimal> prev) {
+    var x = 'A';
+    decimal total = 0;
+
+    foreach (var b in bs) {
+        total += prev[string.Concat(x, b)];
+        x = b;
+    }
+
+    return total;
+}
+
+Dictionary<string, decimal> GetNextPrices(Dictionary<string, decimal> prev, string allChs) {
+    var cur = new Dictionary<string, decimal>();
+
+    foreach (var from in allChs) {
+        foreach (var to in allChs) {
+            decimal best = decimal.MaxValue;
+
+            foreach (var bs in GetDirs(from, to)) {
+
+                var pr = TotalPrice(new string([.. bs, 'A']), prev);
+                best = Math.Min(best, pr);
+            }
+
+            cur[string.Concat(from, to)] = best;
+        }
+    }
+
+    return cur;
+}
+
+int NumPrefix(string s) => int.Parse(s.Substring(0, 3));
+
+decimal Complexity(string[] lines, int times) {
+    var prices = GetTrivialPrices();
+
+    for (int i = 0; i < times; i++)
+        prices = GetNextPrices(prices, AllDirs);
+
+    prices = GetNextPrices(prices, AllNums);
+
+    return lines.Sum(x => NumPrefix(x) * TotalPrice(x, prices));
+}
+
+void Run(string[] input) {
+    var p1 = Complexity(input, 2);
+    var p2 = Complexity(input, 25);
+
+    Console.WriteLine($"part 1 = {p1}");
+    Console.WriteLine($"part 2 = {p2}");
+}
