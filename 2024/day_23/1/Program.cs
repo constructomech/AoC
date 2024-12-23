3eb3d0023c4d9698b9e22ce6ae1404ec4c82d92e
Michael Ott <mott@Mac.local>
2024-12-22 23:41:14 -0800

    2024 Day 23

---
+++ 2024/day_23/1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+
+    var connected = input.Select(s => s.Split('-').ToList()).ToList();
+    var all = new HashSet<string>();
+
+    var connectedTo = new Dictionary<string, List<string>>();
+    foreach (var con in connected) {
+        all.Add(con[0]);
+        all.Add(con[1]);
+        connectedTo.TryAdd(con[0], new List<string>());
+        connectedTo[con[0]].Add(con[1]);
+        connectedTo.TryAdd(con[1], new List<string>());
+        connectedTo[con[1]].Add(con[0]);
+    }
+
+    var setsOfThree = new HashSet<HashSet<string>>();
+    foreach (var permutation in GetPermutations(all, 3)) {
+        var computers = permutation.ToList();
+        var a = computers[0];
+        var b = computers[1];
+        var c = computers[2];
+        
+        if (connectedTo[a].Contains(b) && connectedTo[a].Contains(c) &&
+            connectedTo[b].Contains(a) && connectedTo[b].Contains(c) && 
+            connectedTo[c].Contains(a) && connectedTo[c].Contains(b)) {
+
+            var set = new HashSet<string>() { computers[0], computers[1], computers[2] };
+            setsOfThree.Add(set);
+        }
+    }
+
+    result = setsOfThree.Count(s => s.Where(c => c.StartsWith('t')).Any());
+
+    Console.WriteLine($"Result: {result}");
+}
+
+static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> items, int count) {
+    var i = 0;
+    foreach(var item in items) {
+        if(count == 1) {
+            yield return new T[] { item };
+        }
+        else {
+            foreach(var result in GetPermutations(items.Skip(i + 1), count - 1)) {
+                yield return new T[] { item }.Concat(result);
+            }
+        }
+        i++;
+    }
+}
---
+++ 2024/day_23/2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+bool AllConnected(Dictionary<string, List<string>> connectedTo, IEnumerable<string> set) {
+    foreach (var c0 in set) {
+        foreach (var c1 in set) {
+            if (c0 != c1) {
+                if (!connectedTo[c0].Contains(c1)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+void Run(string[] input) {
+    var connected = input.Select(s => s.Split('-').ToList()).ToList();
+    var all = new HashSet<string>();
+
+    var connectedTo = new Dictionary<string, List<string>>();
+    foreach (var con in connected) {
+        all.Add(con[0]);
+        all.Add(con[1]);
+        connectedTo.TryAdd(con[0], new List<string>());
+        connectedTo[con[0]].Add(con[1]);
+        connectedTo.TryAdd(con[1], new List<string>());
+        connectedTo[con[1]].Add(con[0]);
+    }
+
+    var maxConnections = connectedTo.Max(kvp => kvp.Value.Count);
+
+    var sets = new List<SortedSet<string>>();
+    foreach (var c in all) {
+        sets.Add(new() { c });
+    }
+
+    for (var i = 0; i < sets.Count; i++) {
+
+        foreach (var c in all) {
+            // Add this computer to the set if all computers in the set are connected to each other.
+            if (AllConnected(connectedTo, sets[i].Concat([c]))) {
+                sets[i].Add(c);
+            }
+        }
+    }
+
+    var top = sets.OrderBy(s => -s.Count).First();
+
+    Console.WriteLine($"{string.Join(",", top)}");
+}
