e1f7009d8104b32eebb89cd337fd993f6487db43
Michael Ott <mott@microsoft.com>
2025-01-06 15:39:41 -0800

    2021 day 3

---
+++ 2021/day_03/1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using (StreamReader reader = File.OpenText("input.txt")) {
+
+    var bitsSetToOneCount = new List<int>();
+    int lines = 0;
+
+    while (!reader.EndOfStream) {
+        string? line = reader.ReadLine();
+        if (line != null) {
+            var data = Convert.ToInt32(line, 2);
+
+            while (bitsSetToOneCount.Count < line.Length) {
+                bitsSetToOneCount.Add(0);
+            }
+
+            for (int i = 0; i < line.Length; i++) {
+                if ((data & (1 << i)) != 0) {
+                    bitsSetToOneCount[line.Length - i - 1]++;
+                }
+            }
+
+            lines++;
+        }
+    }
+
+    int gamma = 0;
+    int epison = 0;
+
+    for (var i = 0; i < bitsSetToOneCount.Count; i++) {
+
+        var item = bitsSetToOneCount[i];
+
+        // 1's are more common
+        if (item > (lines / 2)) {
+            gamma |= (1 << (bitsSetToOneCount.Count - i - 1));
+        }
+        else {
+            epison |= (1 << (bitsSetToOneCount.Count - i - 1));
+        }
+    }    
+
+    Console.WriteLine($"Result: {gamma * epison}");
+
+}
+
---
+++ 2021/day_03/2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+int FindValue(IEnumerable<int> inConsideration, int totoalBits, Comparatively preference) {
+    var position = totoalBits - 1;
+    while (inConsideration.Count() > 1) {
+        var testBit = 1 << position;
+        var ones = LinesMatching(inConsideration, testBit, position);
+        var zeros = inConsideration.Except(ones);
+
+        if (ones.Count() >= zeros.Count()) {
+            inConsideration = preference == Comparatively.Greater ? ones : zeros;
+        }
+        else {
+            inConsideration = preference == Comparatively.Greater ? zeros : ones;
+        }
+
+        position--;
+    }
+    return inConsideration.First();
+}
+
+IEnumerable<int> LinesMatching(IEnumerable<int> inConsideration, int testBit, int position) {
+    foreach (var item in inConsideration) {
+        if ((item & (1 << position)) == testBit) {
+            yield return item;
+        }
+    }
+}
+
+using (StreamReader reader = File.OpenText("input.txt")) {
+
+    var originalData = new List<int>();
+    var totalBits = 0;
+
+    while (!reader.EndOfStream) {
+        string? line = reader.ReadLine();
+        if (line != null) {
+            var data = Convert.ToInt32(line, 2);
+            originalData.Add(data);
+
+            if (totalBits < line.Length) {
+                totalBits = line.Length;
+            }
+        }
+    }
+
+    var oxygenGenRating = FindValue(originalData, totalBits, Comparatively.Greater);
+    var co2ScrubberRating = FindValue(originalData, totalBits, Comparatively.Fewer);
+
+    Console.WriteLine($"Result: {oxygenGenRating * co2ScrubberRating}");
+}
+
+enum Comparatively { Fewer, Greater, Equal }
