26343fa679e49aaad7023be54eb84c32d8c7f3bf
Michael Ott <mott@microsoft.com>
2025-01-06 13:45:55 -0800

    2021 day 1

---
+++ 2021/day_01/1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+
+    var nums = input.Select(long.Parse).ToList();
+    var previous = long.MaxValue;
+
+    for (var i = 0; i < nums.Count; i++ ) {
+        var current = nums[i];
+
+        if (current > previous) {
+            result++;
+        }
+
+        previous = current;
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
