f05d25d4edb2b6cd264ee7ad1d671ea34801b937
Michael Ott <mott@microsoft.com>
2025-01-07 13:48:47 -0800

    2021 day 4

--- 2021/day_04/1/Program.cs
+++ 2021/day_04/1/Program.cs
@@ -1,33 +1,27 @@
-﻿using System.Collections.Generic;
-using System.IO;
-
-string[] sequence;
+﻿string[] sequence;
 List<Board> boards;
-List<bool> winning;
 
 using (StreamReader reader = File.OpenText("input.txt")) {
 
-    string rawSequence = reader.ReadLine();
+    var rawSequence = reader.ReadLine()!;
     sequence = rawSequence.Split(',');
 
     boards = new List<Board>();
-    winning = new List<bool>();
-
+ 
     do
     {
         reader.ReadLine(); // Blank line
         Board board = new Board();
 
         for (int y = 0; y < Board.BOARD_SIZE; y++) {
-            string line = reader.ReadLine();
+            string line = reader.ReadLine()!;
             for (int x = 0; x < Board.BOARD_SIZE; x++) {
                 string stringValue = line.Substring(x * 3, 2);
-                 board.setAt(x, y, Convert.ToInt32(stringValue));
+                 board[x, y] = (Convert.ToInt32(stringValue), false);
             }
         }
         
         boards.Add(board);
-        winning.Add(false);
 
     } while (!reader.EndOfStream);
 }
@@ -35,59 +29,39 @@ using (StreamReader reader = File.OpenText("input.txt")) {
 foreach (string rawNum in sequence) {
     int num = Convert.ToInt32(rawNum);
 
-    for (int i = 0; i < boards.Count; i++) {
-        Board board = boards[i];
+    foreach (var board in boards) {
         bool bingo = board.Call(num);
         if (bingo) {
-            winning[i] = true;
-
-            if (allWinning(winning)) {
-                int score = board.getScore();
-                Console.WriteLine("BINGO, score is: {0}, answer is {1}", score, score * num);
-                return;
-            }
+            int score = board.getScore();
+            Console.WriteLine("BINGO, score is: {0}, answer is {1}", score, score * num);
+            return;
         }
     }
-}
-
-bool allWinning(List<bool> list) {
-    foreach (bool item in list) {
-        if (item == false) return false;
-    }
-    return true;
-}
-
-class Cell {
-    public int value = -1;
-    public bool called = false;
 }
 
 class Board {
     public const int BOARD_SIZE = 5;
     
     public Board() {
-        _cells = new Cell[BOARD_SIZE * BOARD_SIZE];
+        _cells = new (int value, bool called)[BOARD_SIZE * BOARD_SIZE];
     }
 
-    public void setAt(int x, int y, int value) {
-        _cells[x + y * BOARD_SIZE] = new Cell() { value = value, called = false };
-    }
-
-    public Cell getAt(int x, int y) {
-        return _cells[x + y * BOARD_SIZE];
+    public (int value, bool called) this[int x, int y] {
+        get { var cell = _cells[x + y * BOARD_SIZE]; return (cell.value, cell.called); }
+        set { _cells[x + y * BOARD_SIZE] = value; }
     }
 
     public bool Call(int number) {
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
-                Cell cell = getAt(i, j);
+                var cell = this[i, j];
                 if (cell.value == number) {
                     cell.called = true;
+                    this[i, j] = cell;
 
                     bool bingoHorizonal = true;
                     for (int x = 0; x < BOARD_SIZE; x++) {
-                        cell = getAt(x, j);
-                        if (cell.called == false) {
+                        if (this[x, j].called == false) {
                             bingoHorizonal = false;
                             break;
                         }
@@ -95,8 +69,7 @@ class Board {
 
                     bool bingoVertical = true;
                     for (int y = 0; y < BOARD_SIZE; y++) {
-                        cell = getAt(i, y);
-                        if (cell.called == false) {
+                        if (this[i, y].called == false) {
                             bingoVertical = false;
                             break;
                         }
@@ -115,7 +88,7 @@ class Board {
         int result = 0;
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
-                Cell cell = getAt(i, j);
+                var cell = this[i, j];
                 if (!cell.called) {
                     result += cell.value;
                 }
@@ -124,7 +97,7 @@ class Board {
         return result;
     }
 
-    Cell[] _cells; 
+    (int value, bool called)[] _cells; 
 }
 
 
--- 2021/day_04/2/Program.cs
+++ 2021/day_04/2/Program.cs
@@ -1,30 +1,30 @@
-﻿using System.Collections.Generic;
-using System.IO;
-
-string[] sequence;
+﻿string[] sequence;
 List<Board> boards;
+List<bool> winning;
 
 using (StreamReader reader = File.OpenText("input.txt")) {
 
-    string rawSequence = reader.ReadLine();
+    var rawSequence = reader.ReadLine()!;
     sequence = rawSequence.Split(',');
 
     boards = new List<Board>();
- 
+    winning = new List<bool>();
+
     do
     {
         reader.ReadLine(); // Blank line
         Board board = new Board();
 
         for (int y = 0; y < Board.BOARD_SIZE; y++) {
-            string line = reader.ReadLine();
+            string line = reader.ReadLine()!;
             for (int x = 0; x < Board.BOARD_SIZE; x++) {
                 string stringValue = line.Substring(x * 3, 2);
-                 board.setAt(x, y, Convert.ToInt32(stringValue));
+                board[x, y] = (Convert.ToInt32(stringValue), false);
             }
         }
         
         boards.Add(board);
+        winning.Add(false);
 
     } while (!reader.EndOfStream);
 }
@@ -32,47 +32,51 @@ using (StreamReader reader = File.OpenText("input.txt")) {
 foreach (string rawNum in sequence) {
     int num = Convert.ToInt32(rawNum);
 
-    foreach (var board in boards) {
+    for (int i = 0; i < boards.Count; i++) {
+        Board board = boards[i];
         bool bingo = board.Call(num);
         if (bingo) {
-            int score = board.getScore();
-            Console.WriteLine("BINGO, score is: {0}, answer is {1}", score, score * num);
-            return;
+            winning[i] = true;
+
+            if (allWinning(winning)) {
+                int score = board.getScore();
+                Console.WriteLine("BINGO, score is: {0}, answer is {1}", score, score * num);
+                return;
+            }
         }
     }
 }
 
-class Cell {
-    public int value = -1;
-    public bool called = false;
+bool allWinning(List<bool> list) {
+    foreach (bool item in list) {
+        if (item == false) return false;
+    }
+    return true;
 }
 
 class Board {
     public const int BOARD_SIZE = 5;
     
     public Board() {
-        _cells = new Cell[BOARD_SIZE * BOARD_SIZE];
+        _cells = new (int value, bool called)[BOARD_SIZE * BOARD_SIZE];
     }
 
-    public void setAt(int x, int y, int value) {
-        _cells[x + y * BOARD_SIZE] = new Cell() { value = value, called = false };
-    }
-
-    public Cell getAt(int x, int y) {
-        return _cells[x + y * BOARD_SIZE];
+    public (int value, bool called) this[int x, int y] {
+        get { var cell = _cells[x + y * BOARD_SIZE]; return (cell.value, cell.called); }
+        set { _cells[x + y * BOARD_SIZE] = value; }
     }
 
     public bool Call(int number) {
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
-                Cell cell = getAt(i, j);
+                var cell = this[i, j];
                 if (cell.value == number) {
                     cell.called = true;
+                    this[i, j] = cell;
 
                     bool bingoHorizonal = true;
                     for (int x = 0; x < BOARD_SIZE; x++) {
-                        cell = getAt(x, j);
-                        if (cell.called == false) {
+                        if (this[x, j].called == false) {
                             bingoHorizonal = false;
                             break;
                         }
@@ -80,8 +84,7 @@ class Board {
 
                     bool bingoVertical = true;
                     for (int y = 0; y < BOARD_SIZE; y++) {
-                        cell = getAt(i, y);
-                        if (cell.called == false) {
+                        if (this[i, y].called == false) {
                             bingoVertical = false;
                             break;
                         }
@@ -100,7 +103,7 @@ class Board {
         int result = 0;
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
-                Cell cell = getAt(i, j);
+                var cell = this[i, j];
                 if (!cell.called) {
                     result += cell.value;
                 }
@@ -109,7 +112,7 @@ class Board {
         return result;
     }
 
-    Cell[] _cells; 
+    (int value, bool called)[] _cells; 
 }
 
 
