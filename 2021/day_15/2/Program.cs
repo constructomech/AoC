7c0c5c3018cb1cd1b1162f9f91ac0d2eceb0b65a
Michael Ott <mott@microsoft.com>
2025-01-08 14:19:49 -0800

    2021 day 15

--- 2021/day_15/2/Program.cs
+++ 2021/day_15/2/Program.cs
@@ -1,10 +1,11 @@
-﻿
+﻿using VCSKicksCollection;
+
 List<string> lines = new List<string>();
 
 using (StreamReader reader = File.OpenText("input.txt"))
 {
     while (!reader.EndOfStream) {
-        string line = reader.ReadLine();
+        string? line = reader.ReadLine();
         if (line != null) {
             lines.Add(line);
         }
@@ -29,11 +30,7 @@ for (int tileX = 0; tileX < 5; tileX++) {
                 int increase = tileX + tileY;
 
                 int newValue = tile[x,y] + increase;
-                while (newValue > 9) newValue -= 9;
-
-                if (newValue == 0) {
-                    Console.WriteLine("whoops");
-                }
+                newValue = (newValue - 1) % 9 + 1;
 
                 matrix[tileX * tile.GetLength(0) + x, tileY * tile.GetLength(1) + y] = (byte)newValue;
             }
@@ -48,57 +45,41 @@ for (int tileX = 0; tileX < 5; tileX++) {
 //     Console.WriteLine();
 // }
 
-int[,] bestScores = new int[matrix.GetLength(0), matrix.GetLength(1)];
-for (int x = 0; x < bestScores.GetLength(0); x++) {
-    for (int y = 0; y < bestScores.GetLength(1); y++) {
-        bestScores[x,y] = int.MaxValue;
-    }
-}
-
-int extreme = 0;
-
-int bestScore = int.MaxValue;
-Path bestPath = null;
-
 Path originPath = new Path() { x = 0, y = 0 };
 
-Queue<Path> queue = new Queue<Path>();
+var queue = new PriorityQueue<Path>();
+
 queue.Enqueue(originPath);
 
-List<Tuple<int, int>> directions = new List<Tuple<int, int>>() { 
-    new Tuple<int, int>(1, 0),
-    new Tuple<int, int>(0, 1),
-    new Tuple<int, int>(-1, 0),
-    new Tuple<int, int>(0, -1),
+(int x, int y)[] directions = new(int, int)[] { 
+    (1, 0),
+    (0, 1),
+    (-1, 0),
+    (0, -1),
 };
+
+const byte VISITED = byte.MaxValue;
 
 while (queue.Count > 0) {
     Path current = queue.Dequeue();
 
-    int currentExtreme = current.x + current.y;
-    if (currentExtreme > extreme) {
-        Console.WriteLine("New extreme: {0} at ({1}, {2})", currentExtreme, current.x, current.y);
-        extreme = currentExtreme;
+    // If we happen to be at the exit, see if we should update the best path.    
+    if (current.x == matrix.GetLength(0) - 1 && current.y == matrix.GetLength(1) - 1) {
+        Console.WriteLine("Best score: {0}", current.cost);
+        break;
     }
 
-    if (current.cost < bestScore && current.cost < bestScores[current.x, current.y]) {
-        bestScores[current.x, current.y] = current.cost;
+    if (matrix[current.x, current.y] != VISITED) {
 
-        // If we happen to be at the exit, see if we should update the best path.    
-        if (current.x == matrix.GetLength(0) - 1 && current.y == matrix.GetLength(1) - 1) {
+        matrix[current.x, current.y] = VISITED;
 
-            bestScore = current.cost;
-            bestPath = current;
+        foreach (var direction in directions) {
+            (int x, int y) targetPos = (current.x + direction.x, current.y + direction.y);
 
-            Console.WriteLine("New best score: {0}", bestScore);
-        }
-        else 
-        {
-            foreach (var direction in directions) {
-                int x = current.x + direction.Item1;
-                int y = current.y + direction.Item2;
-                if (x >= 0 && x < matrix.GetLength(0) && y >= 0 && y < matrix.GetLength(1)) {
-                    Path newPath = new Path() { x = x, y = y, parent = current, cost = current.cost + matrix[x, y]};
+            if (targetPos.x >= 0 && targetPos.x < matrix.GetLength(0) && targetPos.y >= 0 && targetPos.y < matrix.GetLength(1)) {
+
+                if (matrix[targetPos.x, targetPos.y] != VISITED) {
+                    Path newPath = new Path() { x = targetPos.x, y = targetPos.y, parent = current, cost = current.cost + matrix[targetPos.x, targetPos.y]};
                     queue.Enqueue(newPath);
                 }
             }
@@ -106,9 +87,11 @@ while (queue.Count > 0) {
     }
 }
 
-Console.WriteLine("Best Score: {0}", bestScore);
+class Path : IComparable<Path> {
 
-class Path {
+    public int CompareTo(Path other) {
+        return cost.CompareTo(other.cost);
+    }
 
     public int x;
     public int y;
@@ -116,5 +99,4 @@ class Path {
     public Path parent = null;
 
     public int cost = 0;
-
 }
