7b4bd2863a6e72238ef2746da18a4af36f0e3659
Michael Ott <mott@microsoft.com>
2025-01-08 14:16:36 -0800

    2021 day 14

--- 2021/day_14/2/Program.cs
+++ 2021/day_14/2/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.Text;
 
 string template;
-var insertionRules = new Dictionary<string, char>();
+byte[] insertionRules = new byte[(int)Math.Pow(2, 16)];
+var histogramCache = new Dictionary<int, long[]>();
 
 using (StreamReader reader = File.OpenText("input.txt"))
 {
@@ -12,63 +13,87 @@ using (StreamReader reader = File.OpenText("input.txt"))
     while (!reader.EndOfStream) {
         line = reader.ReadLine();
         string[] parts = line.Split(" -> ");
-        insertionRules.Add(parts[0], parts[1][0]);
+        char c1 = parts[0][0];
+        char c2 = parts[0][1];
+        char o = parts[1][0];
+        int idx = ((byte)(c1 - 'A') << 8) + (byte)(c2 -'A');
+        insertionRules[idx] = (byte)o;
     }
 }
 
-var histogram = new Dictionary<char, long>();
+var histogram = new long[(int)Math.Pow(2, 8)];
 foreach (char c in template) {
     addTo(histogram, c);
 }
 
-var stack = new Stack<Tuple<string, int>>();
+int iteration = 0;
+
 for (int i = 0; i < template.Length - 1; i++) {
     string pair = template.Substring(i, 2);
-    stack.Push(new Tuple<string, int>(pair, 0));
-}
-
-while (stack.Count > 0) {
-    (string pair, int depth) = stack.Pop();
-
-    if (depth < 40) {
-        char c;
-        if (insertionRules.TryGetValue(pair, out c)) {
-            addTo(histogram, c);
-
-            string newPair1 = pair[0].ToString() + c;
-            string newPair2 = c + pair[1].ToString();
-
-            stack.Push(new Tuple<string, int>(newPair1, depth + 1));
-            stack.Push(new Tuple<string, int>(newPair2, depth + 1));
-        }
-    }
+    byte c1 = (byte)pair[0];
+    byte c2 = (byte)pair[1];
+    computeHistogram(histogram, c1, c2, 0);
 }
 
 printHistogram(histogram);
 
 
-void printHistogram(Dictionary<char, long> histogram) {
-    foreach ((char c, long count) in histogram) {
-        Console.WriteLine("{0}: {1}", c, count);
+void computeHistogram(long[] histogram, byte c1, byte c2, byte depth) {
+    if (++iteration % 10000 == 0) {
+        Console.WriteLine("Iteration {0}", iteration);
+    }
+    if (depth < 40) {
+        var result = getCachedHistogram(c1, c2, depth);
+        if (result == null) {
+            result = new long[256];
+
+            int idx = ((byte)(c1 - 'A') << 8) + (byte)(c2 -'A');
+            byte insertionResult = insertionRules[idx];
+            if (insertionResult != 0) {
+                char c = (char)insertionResult;
+                addTo(result, c);
+
+                int nextDepth = depth + 1;
+                computeHistogram(result, c1, insertionResult, (byte)(depth + 1));
+                computeHistogram(result, insertionResult, c2, (byte)(depth + 1));
+            }
+
+            addHistogramToCache(c1, c2, depth, result);
+        }
+        addInto(histogram, result);
     }
 }
 
-Dictionary<char, long> computeHistogram(string polimer) {
-    var histogram = new Dictionary<char, long>();
-    foreach (char c in polimer) {
-        if (histogram.ContainsKey(c)) { 
-            histogram[c]++;
-        } else {
-            histogram.Add(c, 1);
+void printHistogram(long[] histogram) {
+    for (int i = 0; i < byte.MaxValue; i++) {
+        long result = histogram[i];
+        if (histogram[i] != 0) {
+            Console.WriteLine("{0}: {1}", (char)i, result);
         }
     }
+}
+
+void addTo(long[] histogram, char c) {
+    histogram[(byte)c]++;
+}
+
+void addInto(long[] dest, long[] source) {
+    for (int i = 0; i < byte.MaxValue; i++) {
+        dest[i] += source[i];
+    }
+}
+
+long[] getCachedHistogram(byte c1, byte c2, byte depth) {
+    int key = (depth << 16) + (c1 << 8) + c2;
+
+    long[] histogram = null;
+    histogramCache.TryGetValue(key, out histogram);
+
     return histogram;
 }
 
-void addTo(Dictionary<char, long> dest, char c) {
-    if (dest.ContainsKey(c)) {
-        dest[c] += 1;
-    } else {
-        dest.Add(c, 1);
-    }
+void addHistogramToCache(byte c1, byte c2, byte depth, long[] histogram) {
+    var copy = new long[(int)Math.Pow(2, 8)];
+    Array.Copy(histogram, copy, (int)Math.Pow(2, 8));
+    histogramCache.Add((depth << 16) + (c1 << 8) + c2, histogram);
 }
