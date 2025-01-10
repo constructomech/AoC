a6104c7997b2d6e1b557bec92ed5a07b8e45b5c0
Michael Ott <mott@microsoft.com>
2025-01-10 15:16:33 -0800

    2021 day 23 - WIP

--- 2021/day_23/1'/Program.cs
+++ 2021/day_23/1'/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Immutable;
 using System.Data;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 
 Stopwatch watch = new Stopwatch();
@@ -13,21 +14,44 @@ Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
 
 void Run(string[] input) {
     var result = 0L;
+    var bestKnownCosts = new Dictionary<ImmutableArray<(Vec2 pos, char type)>, int>();
 
-    var board = AmphipodBurrow.FromString(input);
+    (var board, var amphipodsInitialState) = AmphipodBurrow.FromString(input);
 
-    board.Print();
+    var q = new PriorityQueue<(ImmutableArray<(Vec2 pos, char type)> amphipods, int knownCost), int>();
+    q.Enqueue((amphipodsInitialState, 0), board.EstimateOfCostToGoal(amphipodsInitialState));
+
+    while (q.TryPeek(out var item, out var costEstimate)) {
+        q.Dequeue();
+        bestKnownCosts[item.amphipods] = item.knownCost;
+
+        Console.WriteLine($"Known cost: {item.knownCost}, Estimated total: {costEstimate}, Queued: {q.Count}");
+//        board.Print(item.amphipods);
+
+        var roomMoves = board.AvailableRoomMoves(item.amphipods);
+        var hallwayMoves = board.AvailableHallwayMoves(item.amphipods);
+        var moves = roomMoves.Concat(hallwayMoves);
+
+        foreach (var move in moves) {
+            var newAmphipods = item.amphipods.Select(p => p.pos == move.from ? (move.to, p.type) : p).ToImmutableArray();
+            var knownCost = item.knownCost + move.cost;
+            var estimatedRemainingCost = board.EstimateOfCostToGoal(newAmphipods);
+            var totalCost = knownCost + estimatedRemainingCost;
+
+            // Check if the cost < best total cost for this board state before queuing anything
+            if (bestKnownCosts.TryGetValue(newAmphipods, out var bestKnownCost) && bestKnownCost <= knownCost) {
+                continue;
+            }
+
+            q.Enqueue((newAmphipods, knownCost), totalCost);
+        }
+    }
 
     Console.WriteLine($"Result: {result}");
 }
 
 
 public record Vec2 (int X, int Y) {
-    public static Vec2 FromString(string s) {
-        var parts = s.Split(',');
-        return new(int.Parse(parts[0]), int.Parse(parts[1]));
-    }
-
     public static Vec2 operator +(Vec2 a, Vec2 b) => new(a.X + b.X, a.Y + b.Y);
 
     public static Vec2 operator -(Vec2 a, Vec2 b) => new(a.X - b.X, a.Y - b.Y);
@@ -35,71 +59,69 @@ public record Vec2 (int X, int Y) {
     public static Vec2 operator *(Vec2 a, int b) => new(a.X * b, a.Y * b);
 }
 
-// TODO:
-// Externalize amphipods from board so we can queue and cache that state.
-//
+
 public class AmphipodBurrow : FixedBoard<char> {
 
     public AmphipodBurrow(int width, int height) : base(width, height) {
         _hallway = new List<Vec2>();
         _rooms = new List<List<Vec2>>();
-        _amphipods = new List<(Vec2 pos, char type)>();
     }
 
     public List<List<Vec2>> Rooms { get => _rooms; }
 
     public List<Vec2> Hallway { get => _hallway; }
 
-    public List<(Vec2, int)> AvailableRoomMoves() {
-        var moves = new List<(Vec2, int)>();
-        
-        foreach (var pos in this.Hallway) {
-            var candidate = this._amphipods.Where(p => p.pos == pos);
-            if (candidate.Any()) {
-                var amphipod = candidate.First();
-                var targetRoom = this.Rooms.Where(r => r.Contains(pos)).First();
+    public int EstimateOfCostToGoal(ImmutableArray<(Vec2 pos, char type)> amphipods) {
+        var cost = 0;
+        foreach (var amphipod in amphipods) {
+            var targetRoom = this.Rooms[amphipod.type - 'A'];
 
-                // Build an IEnumerable<Vec2> of the path to a valid target room location
-                // Enumerable.Range(amphipod.pos.X, targetRoom.
-                // var path = 
-            } 
+            var path0 = Enumerable.Range(amphipod.pos.Y, this.Hallway[0].Y - 1).Select(y => new Vec2(targetRoom[0].X, y));
+            var path1 = Enumerable.Range(amphipod.pos.X, targetRoom[0].X - 1).Select(x => new Vec2(x, this.Hallway[0].Y));
+            var path2 = Enumerable.Range(this.Hallway[0].Y, targetRoom[0].Y).Select(y => new Vec2(targetRoom[0].X, y));
+            var path = path0.Concat(path1).Concat(path2);
+
+            cost += CostPerMove(amphipod.type) * path.Count();
         }
-
-        return moves;
+        return cost;
     }
 
-    public List<(Vec2 from, Vec2 to, int cost)> AvailableHallwayMoves() {
+    public List<(Vec2 from, Vec2 to, int cost)> AvailableRoomMoves(ImmutableArray<(Vec2 pos, char type)> amphipods) {
         var moves = new List<(Vec2 from, Vec2 to, int cost)>();
-        var hallwayY = this.Hallway[0].Y;
 
-        foreach (var pos in this.Rooms.SelectMany(r => r)) {
-            if (this[pos] != '.') {
+        var candidates = amphipods.Where(p => this.Hallway.Contains(p.pos));
+        
+        foreach (var candidate in candidates) {
+            var targetRoomSet = this.Rooms.Where(r => r.Contains(candidate.pos));
+            if (targetRoomSet.Any()) {
 
-                // If the amphipod isn't blocked in the room
-                if (!this._amphipods.Any(p => p.pos == new Vec2(pos.X, pos.Y - 1) || p.pos == new Vec2(pos.X, hallwayY))) {
+                var targetRoom = targetRoomSet.First();
 
-                    Func<List<(Vec2, Vec2, int)>, int, Vec2, int, bool> conditionalAdd = (moves, offset, pos, hallwayY) => {
-                        // If this is not a position directly above a room
-                        if (this[pos.X + offset, hallwayY + 1] != '.') {
+                var path0 = Enumerable.Range(candidate.pos.X, targetRoom[0].X - 1).Select(x => new Vec2(x, this.Hallway[0].Y));
+                var path1 = Enumerable.Range(this.Hallway[0].Y, targetRoom[0].Y).Select(y => new Vec2(targetRoom[0].X, y));
+                var path = path0.Concat(path1);
 
-                            // If this position is blocked by another amphipod, bail
-                            if (this._amphipods.Any(p => p.pos == new Vec2(pos.X + offset, hallwayY))) return false;
+                var squatter = amphipods.Where(p => p.pos == path.Last());
+                if (squatter.Any()) {
+                    if (squatter.First().type == candidate.type) {
+                        path = path.Take(path.Count() - 1);
+                    }
+                    else {
+                        continue;
+                    }
+                }
 
-                            var cost = AmphipodBurrow.ManhattanDistance(pos, new Vec2(pos.X + offset, hallwayY));
+                bool pathIsClear = true;
+                foreach (var pos in path) {
+                    if (amphipods.Any(p => p.pos == pos)) {
+                        pathIsClear = false;
+                        break;
+                    }
+                }
 
-                            moves.Add((pos, new Vec2(pos.X + offset, hallwayY), cost));
-                        }
-                        return true;
-
-                    };
-
-                    // Moves to the left
-                    var offset = -1;
-                    while (this[pos.X + offset, hallwayY] == '.' && conditionalAdd(moves, offset, pos, hallwayY)) offset--;
-                    
-                    // Moves to the right
-                    offset = 1;
-                    while (this[pos.X + offset, hallwayY] == '.' && conditionalAdd(moves, offset, pos, hallwayY)) offset++;
+                if (pathIsClear) {
+                    var cost = path.Select(p => CostPerMove(this[p])).Sum();
+                    moves.Add((candidate.pos, targetRoom[0], cost));
                 }
             }
         }
@@ -107,7 +129,52 @@ public class AmphipodBurrow : FixedBoard<char> {
         return moves;
     }
 
-    public void Print() => Print(c => c);
+    public List<(Vec2 from, Vec2 to, int cost)> AvailableHallwayMoves(ImmutableArray<(Vec2 pos, char type)> amphipods) {
+        var moves = new List<(Vec2 from, Vec2 to, int cost)>();
+        var hallwayY = this.Hallway[0].Y;
+
+        var candidates = amphipods.Where(p => this.Rooms.SelectMany(r => r).Contains(p.pos));
+
+        //TODO: NEED TO SUBTRACT CANDIDATES THAT ARE ALREADY IN THE CORRECT ROOM
+
+        foreach (var candidate in candidates) {
+
+            // If the amphipod isn't blocked in the room
+            if (!amphipods.Any(p => p.pos == new Vec2(candidate.pos.X, candidate.pos.Y - 1) || p.pos == new Vec2(candidate.pos.X, hallwayY))) {
+
+                Func<List<(Vec2, Vec2, int)>, int, Vec2, int, bool> conditionalAdd = (moves, offset, pos, hallwayY) => {
+                    // If this is not a position directly above a room
+                    if (this[pos.X + offset, hallwayY + 1] != '.') {
+
+                        // If this position is blocked by another amphipod, bail
+                        if (amphipods.Any(p => p.pos == new Vec2(pos.X + offset, hallwayY))) return false;
+
+                        var cost = AmphipodBurrow.ManhattanDistance(pos, new Vec2(pos.X + offset, hallwayY)) * CostPerMove(candidate.type);
+
+                        moves.Add((pos, new Vec2(pos.X + offset, hallwayY), cost));
+                    }
+                    return true;
+
+                };
+
+                // Moves to the left
+                var offset = -1;
+                while (this[candidate.pos.X + offset, hallwayY] == '.' && conditionalAdd(moves, offset, candidate.pos, hallwayY)) offset--;
+               
+                // Moves to the right
+                offset = 1;
+                while (this[candidate.pos.X + offset, hallwayY] == '.' && conditionalAdd(moves, offset, candidate.pos, hallwayY)) offset++;
+            }
+        }
+        return moves;
+    }
+
+    public void Print(ImmutableArray<(Vec2 pos, char type)> amphipods) => Print((pos, c) => {
+        if (amphipods.Any(p => p.pos == pos)) {
+            return amphipods.First(p => p.pos == pos).type;
+        }
+        return c;
+    });
 
     public static int CostPerMove(char c) =>
         c switch {
@@ -118,12 +185,14 @@ public class AmphipodBurrow : FixedBoard<char> {
             _ => 0
         };
 
-    public static AmphipodBurrow FromString(string[] input) {
+    public static (AmphipodBurrow board, ImmutableArray<(Vec2 pos, char type)> amphipods) FromString(string[] input) {
 
         var board = new AmphipodBurrow(input.Length > 0 ? input[0].Length : 0, input.Length);
-        board.PopulateBoard(input, c => {
+        var amphipods = new List<(Vec2 pos, char type)>();
+
+        board.PopulateBoard(input, (pos, c) => {
             if (c >= 'A' && c <= 'D') {
-                board._amphipods.Add((new Vec2(board._amphipods.Count, 0), c));
+                amphipods.Add((pos, c));
                 return '.';
             }
             return c;
@@ -133,29 +202,23 @@ public class AmphipodBurrow : FixedBoard<char> {
             board._hallway.Add(new Vec2(x, 1));
         }
 
+        var roomLocations = new List<Vec2>();
         for (var x = 1; x < board.Width - 1; x++) {
             for (var y = 2; y < board.Height - 1; y++) {
                 if (board[x, y] == '.') {
-                    var room = new List<Vec2>();
-                    var offset = 0;
-                    while (board[x - offset, y] == '.') {
-                        room.Add(new Vec2(x - offset, y));
-                        offset++;
-                    }
-                    board._rooms.Add(room);
+                    roomLocations.Add(new Vec2(x, y));
                 }
             }
         }
+        board._rooms = roomLocations.GroupBy(p => p.X).Select(g => g.OrderBy(p => p.Y).ToList()).ToList();
 
-        return board;
+        return (board, amphipods.ToImmutableArray());
     }
 
     private static int ManhattanDistance(Vec2 a, Vec2 b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
 
     private List<List<Vec2>> _rooms;
     private List<Vec2> _hallway;
-
-    private List<(Vec2 pos, char type)> _amphipods;
 }
 
 public class FixedBoard<T> {
@@ -187,169 +250,24 @@ public class FixedBoard<T> {
         }
     }
 
-    public void Print(Func<T, char> resovleChar) => Print(resovleChar, null, default(T));
-    public void Print(Func<T, char> resovleChar, List<Vec2>? overridePositions, T? overrideValue) {
+    public void Print(Func<Vec2, T, char> resovleChar) {
         for (var y = 0; y < this.Height; y++) {
             for (var x = 0; x < this.Width; x++) {
-                char printVal = resovleChar(_data[x, y]);
-                if (overridePositions != null && overridePositions.Contains(new Vec2(x, y))) {
-                    if (overrideValue == null) throw new ArgumentNullException(nameof(overrideValue));
-                    printVal = resovleChar(overrideValue);
-                }
+                char printVal = resovleChar(new Vec2(x, y), _data[x, y]);
                 Console.Write(printVal);
             }
             Console.WriteLine();
         }
     }
 
-    protected void PopulateBoard(string[] input, Func<char, T> transform) {
+    protected void PopulateBoard(string[] input, Func<Vec2, char, T> transform) {
         for (var y = 0; y < this.Height; y++) {
             for (var x = 0; x < this.Width; x++) {
                 var item = input[y].Length > x ? input[y][x] : ' ';
-                this._data[x, y] = transform(item);
+                this._data[x, y] = transform(new Vec2(x, y), item);
             }
         }
     }
 
     private T[,] _data;
-}
-
-public class Graph<TData> {
-    public int VertexCount { get => _verticies.Count; }
-
-    public int AddVertex(TData data) {
-        this._verticies.Add((data, new List<(int vertexIndex, int weight)>()));
-        return this._verticies.Count - 1;
-    }
-
-    public void AddEdge(int lhsVertexIndex, int rhsVertexIndex, int weight) {
-        this._verticies[lhsVertexIndex].edges.Add((rhsVertexIndex, weight));
-    }
-
-    public (TData data, List<(int vertexIndex, int weight)> edges) GetVertex(int index) {
-        return _verticies[index];
-    }
-
-    IEnumerable<(TData data, List<(int vertexIndex, int weight)> edges)> SelectVerticies(Func<TData, List<(int vertexIndex, int weight)>, bool> predicate) {
-        foreach (var vertex in _verticies) {
-            if (predicate(vertex.data, vertex.edges)) {
-                yield return vertex;
-            }
-        }
-    }
-
-    private List<(TData data, List<(int vertexIndex, int weight)> edges)> _verticies = 
-        new List<(TData data, List<(int vertexIndex, int weight)> edges)>();
-}
-
-public class DijkstraAlgorithm<TData> {
-    public static int[] FindMinWeightFromSource(Graph<TData> graph, int source) {
-        var vertices = graph.VertexCount;
-        var distances = new int[vertices];
-        var shortestPathTreeSet = new bool[vertices];
-
-        for (int i = 0; i < vertices; i++) {
-            distances[i] = int.MaxValue;
-            shortestPathTreeSet[i] = false;
-        }
-
-        distances[source] = 0;
-
-        for (int count = 0; count < vertices - 1; count++) {
-
-            int u = MinimumDistance(distances, shortestPathTreeSet);
-            shortestPathTreeSet[u] = true;
-
-            foreach ((int v, int weight) in graph.GetVertex(u).edges) {
-
-                if (!shortestPathTreeSet[v] && distances[u] != int.MaxValue && distances[u] + weight < distances[v]) {
-                    distances[v] = distances[u] + weight;
-                }
-            }
-        }
-
-        return distances;
-    }
-
-    private static int MinimumDistance(int[] distances, bool[] shortestPathTreeSet) {
-        int min = int.MaxValue, minIndex = -1;
-
-        for (int v = 0; v < distances.Length; v++) {
-
-            if (!shortestPathTreeSet[v] && distances[v] <= min) {
-                min = distances[v];
-                minIndex = v;
-            }
-        }
-
-        return minIndex;
-    }
-}
-
-public static class AStar<TData> {
-
-    // Find and return the shortest path from start to goal.
-    public static List<int>? Search(Graph<TData> graph, int startIdx, int goalIdx, Func<int, int, int> heuristic) {
-
-        var openList = new List<int> { startIdx };
-        var closedList = new HashSet<int>();
-
-        // Dictionaries to hold g(n), h(n), and parent pointers
-        var gScore = new Dictionary<int, double> { [startIdx] = 0 };
-        var hScore = new Dictionary<int, double> { [startIdx] = heuristic(startIdx, goalIdx) };
-        var parentMap = new Dictionary<int, int>();
-
-        while (openList.Count > 0) {
-
-            // Find node in open list with the lowest F score
-            var currentIdx = openList.OrderBy(id => gScore[id] + hScore[id]).First();
-
-            if (currentIdx == goalIdx) {
-                return ReconstructPath(parentMap, currentIdx);
-            }
-
-            openList.Remove(currentIdx);
-            closedList.Add(currentIdx);
-
-            foreach ((var neighborIdx, var weightToNeighbor) in graph.GetVertex(currentIdx).edges) {
-
-                var neighbor = graph.GetVertex(neighborIdx);
-                if (closedList.Contains(neighborIdx)) continue;
-
-                // Tentative gScore (current gScore + distance to neighbor)
-                double tentativeGScore = gScore[currentIdx] + weightToNeighbor;
-
-                if (!gScore.ContainsKey(neighborIdx) || tentativeGScore < gScore[neighborIdx])
-                {
-                    // Update gScore and hScore
-                    gScore[neighborIdx] = tentativeGScore;
-                    hScore[neighborIdx] = heuristic(neighborIdx, goalIdx);
-
-                    // Set the current node as the parent of the neighbor
-                    parentMap[neighborIdx] = currentIdx;
-
-                    if (!openList.Contains(neighborIdx))
-                    {
-                        openList.Add(neighborIdx);
-                    }
-                }
-            }
-        }
-
-        return null; // No path found
-    }
-
-    private static List<int> ReconstructPath(Dictionary<int, int> parentMap, int current)
-    {
-        var path = new List<int> { current };
-        
-        while (parentMap.ContainsKey(current))
-        {
-            current = parentMap[current];
-            path.Add(current);
-        }
-        
-        path.Reverse();
-        return path;
-    }
 }
