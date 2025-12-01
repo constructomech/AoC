3b831a2a69b3575b30d9afb1f53f6eaa77907dd5
Michael Ott <michael@catsmeow.me>
2025-11-30 21:39:19 -0800

    2025 day 1 -- Why can't I get math to work?

---
+++ 2025/day_01/1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+    var dial = 50L;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        Direction dir = (input[i][0] == 'R') ? Direction.Right : Direction.Left;
+        int steps = int.Parse(input[i].Substring(1));
+
+        if (dir == Direction.Right)
+        {
+            dial += steps;
+        }
+        else
+        {
+            dial -= steps;
+        }
+
+        if (dial == 0)
+        {
+            result++;
+        }
+        while (dial < 0) { dial += 100; }
+        while (dial >= 100 ) { dial -= 100; }
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
+
+enum Direction { Up, Down, Left, Right }
---
+++ 2025/day_01/2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+    var dial = 50L;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        var inc = (input[i][0] == 'R') ? 1 : -1;
+        int steps = int.Parse(input[i].Substring(1));
+
+        while (steps > 0) { 
+            dial += inc;
+            if (dial == 100) { dial = 0; }
+            else if (dial == -1) { dial = 99; }
+            if (dial == 0) result++;
+            steps--;
+        }
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
