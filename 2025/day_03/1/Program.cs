4f4c8d555b1797a07f719f3fa59b403266271dff
Michael Ott <michael@catsmeow.me>
2025-12-02 21:32:27 -0800

    2025 day 3

---
+++ 2025/day_03/1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+
+    foreach (var line in input)
+    {
+        var firstIdx = 0;
+        var secondIdx = line.Length - 1;
+
+        for (int i = 1; i < line.Length - 1; i++)
+        {
+            if (line[i] > line[firstIdx])
+            {
+                firstIdx = i;
+            }
+        }
+
+        for (int j = line.Length - 2; j > firstIdx; j--)
+        {
+            if (line[j] > line[secondIdx])
+            {
+                secondIdx = j;
+            }
+        }
+
+        var joltage = Convert.ToInt64(line[firstIdx].ToString() + line[secondIdx].ToString());
+        result += joltage;
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
---
+++ 2025/day_03/2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+
+    foreach (var line in input)
+    {
+        var digits = new char[12];
+        Array.Fill(digits, '0');
+        var inputIdx = 0;
+
+        for (var outputIdx = 0; outputIdx < 12; outputIdx++)
+        {
+            digits.Append('0');
+            
+            for (int i = inputIdx; i <= line.Length - 12 + outputIdx; i++)
+            {
+                if (line[i] > digits[outputIdx])
+                {
+                    digits[outputIdx] = line[i];
+                    inputIdx = i + 1;
+                }
+            }
+        }
+
+        var digitsStr = new string(digits);
+        var joltage = Convert.ToInt64(digitsStr);
+
+        result += joltage;
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
