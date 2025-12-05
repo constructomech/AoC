64ca1fce761349b3197d146039e0b9098e1471d1
Michael Ott <michael@catsmeow.me>
2025-12-04 23:36:57 -0800

    2025 day 5

---
+++ 2025/day_05/1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+
+    var i = 0;
+    var ranges = new List<(long from, long to)>();
+    var tests = new List<long>();
+
+    while (true)
+    {
+        if (input[i] == "") break;
+
+        var strs = input[i].Split('-');
+        ranges.Add((Convert.ToInt64(strs[0]), Convert.ToInt64(strs[1])));
+        i++;
+    }
+
+    i++;
+
+    while (i < input.Length)
+    {
+        tests.Add(Convert.ToInt64(input[i]));
+        i++;
+    }
+
+    foreach (var test in tests)
+    {
+        bool spoiled = true;
+        foreach (var range in ranges)
+        {
+            if (test >= range.from && test <= range.to)
+            {
+                spoiled = false;
+                break;
+            }
+        }
+        if (!spoiled)
+        {
+            result++;
+        }
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
---
+++ 2025/day_05/2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+
+Stopwatch watch = new Stopwatch();
+watch.Start();
+var input = File.ReadAllLines("input.txt");
+Run(input);
+watch.Stop();
+Console.WriteLine($"Completed in {watch.ElapsedMilliseconds}ms");
+
+
+void Run(string[] input) {
+    var result = 0L;
+
+    var i = 0;
+    var ranges = new List<(long from, long to)>();
+
+    while (true)
+    {
+        if (input[i] == "") break;
+
+        var strs = input[i].Split('-');
+        ranges.Add((Convert.ToInt64(strs[0]), Convert.ToInt64(strs[1])));
+        i++;
+    }
+    ranges.Sort((a, b) => a.from.CompareTo(b.from));
+
+    var outputRanges = new List<(long from, long to)>();
+    var current = ranges[0];
+
+    for (int j = 1; j < ranges.Count; j++)
+    {
+        if (ranges[j].from <= current.to)
+        {
+            current = (current.from, Math.Max(current.to, ranges[j].to));
+        }
+        else
+        {
+            outputRanges.Add(current);
+            current = ranges[j];
+        }
+    }
+    outputRanges.Add(current);
+
+    foreach (var range in outputRanges)
+    {
+        result += (range.to - range.from + 1);
+    }
+
+    Console.WriteLine($"Result: {result}");
+}
